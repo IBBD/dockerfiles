df89e56f5896597ffa68df2340b7a7d0341ce8a7
alex cai <caiyingyao@ibbd.net>
2017-07-06 21:32:10 +0800

    start

--- notebook/pyspark/start.sh
+++ notebook/pyspark/start.sh
@@ -4,7 +4,8 @@
 # Author: alex
 # Created Time: 2017年07月06日 星期四 21时08分35秒
 
-docker run -d -p 8888:8888 \
-    -v /var/www/notebook/pyspark/work:/home/jovyan/work
+docker run -d --name=ibbd-pyspark-notebook \
+    -p 8888:8888 \
+    -v /var/www/notebook/pyspark/work:/home/jovyan/work \
     ibbd/pyspark-notebook \
     start-notebook.sh --NotebookApp.token='ibbd-data'
