d4c7f5b95ed80a400e494d458f1bd53cd4710c23
alex <cyy0523xc@gmail.com>
2019-01-02 17:08:29 +0800

    konga

--- kong/konga/start-admin.sh
+++ kong/konga/start-admin.sh
@@ -4,7 +4,7 @@
 # Author: alex
 # Created Time: 2019年01月02日 星期三 10时23分02秒
 # The default `sails-disk` adapter is not designed for use as a production database
-docker run -p 1337:1337 \
+docker run -d -p 1337:1337 \
     --name konga \
     -e "NODE_ENV=development" \
     -e "TOKEN_SECRET=ibbd.net" \
